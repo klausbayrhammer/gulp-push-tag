49e73c74e54c45a784a0bd9386f02fd3a72b9537
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2015-07-23 17:00:59 +0200

    removed dest and added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var git = require('gulp-git');
 var bump = require('gulp-bump');
 var filter = require('gulp-filter');
 var tag_version = require('gulp-tag-version');
+var gulp = require('gulp');
 
 function cb(obj) {
     var stream = new require('stream').Transform({objectMode: true});
@@ -19,7 +20,6 @@ gulp.task('bump_patch', function () {
         .pipe(git.commit('release version'))
         .pipe(filter('package.json'))
         .pipe(tag_version())
-        .pipe(gulp.dest('./'))
         .pipe(cb(function () {
             git.push('origin', 'master', {args: " --tags --verbose"}, function (err) {
                 if (err) {
